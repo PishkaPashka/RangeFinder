0c5a4ffbca985d8e38c17f4d219d5149eee3e9bc
Pavel <pavelv@dmdevelopment.ru>
2023-01-16 16:48:36 +0700

    Add project files.

---
+++ RangeFinder/ComPort.cs
@@ -0,0 +1,16 @@
+﻿namespace RangeFinder
+{
+    public class ComPort
+    {
+        public string Name { get; }
+        public string Device { get; }
+
+        public string FullName => $"{Name} ({Device})";
+
+        public ComPort((string, string) port)
+        {
+            Name = port.Item1;
+            Device = port.Item2;
+        }
+    }
+}
